41059bc77f2a2539adf406bd46bce5cd3fb62d2a
weixuedian <112606652@qq.com>
2019-01-06 12:14:21 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,12 +3,14 @@ if [ ! -d "/home/autodep/haohao/" ];then
  git clone git@gitlab.com:haohaoteam/haohao.git
 else
  echo "文件夹已经存在"
- git pull
 fi
 echo "开始执行中...."
 cd /home/autodep/haohao
 git pull
-echo "更新完毕"
+echo "资源完毕更新完毕"
+
+mvn package -Pdev -DskipTests=true
+echo "执行打包完毕"
 
 echo "结束执行"
 
