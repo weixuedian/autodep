86b2543d91a7e8ddea9b5caaad0c22fafc2b5ceb
weixuedian <sujiansoft@qq.com>
2019-07-15 10:47:31 +0800

    Update build_admin.sh

--- build_admin.sh
+++ build_admin.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
-if [ ! -d "/home/autodep/haohao/" ];then
+if [ ! -d "/home/" ];then
  git clone git@gitlab.com:haohaoteam/haohao.git
 else
  echo "文件夹已经存在"
 fi
 echo "开始执行中...."
-cd /home/autodep/haohao
+cd /home/haohao
 git pull
 echo "资源完毕更新完毕"
 
-mvn package -Pdev -DskipTests=true
+mvn package -Pprod -DskipTests=true
 echo "执行打包完毕"
 rm -rf *.jar
 rm -rf *.war
-find .|grep 0.0.war|xargs -i -t mv {} /home/autodep/haohao/
-find .|grep 0.0.jar|xargs -i -t mv {} /home/autodep/haohao/
+find .|grep 0.0.war|xargs -i -t mv {} /home/haohao/
+find .|grep 0.0.jar|xargs -i -t mv {} /home/haohao/
 echo "移动文件完毕"
 sh ./deploy/haohao-admin-test-shell.sh 
 echo "启动docker 中................................"
